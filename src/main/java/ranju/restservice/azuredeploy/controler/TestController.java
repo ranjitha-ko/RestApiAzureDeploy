07cfc5bceedc9804ec70bb3544d44e9412256952
ranjitha-ko <75278517+ranjitha-ko@users.noreply.github.com>
2021-01-25 12:02:22 -0800

    Update TestController.java

--- src/main/java/ranju/restservice/azuredeploy/controler/TestController.java
+++ src/main/java/ranju/restservice/azuredeploy/controler/TestController.java
@@ -9,7 +9,7 @@ public class TestController {
     @GetMapping("/")
     public String getdata()
     {
-       return "Hello Ranju your azure Deploy is ready";
+       return "Hello Ranju";
     }
 
 }
